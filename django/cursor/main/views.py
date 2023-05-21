f425407bdf95fd069a0044aad5d2e0e5e720cd34
vburdyk <burdikv1991@gmail.com>
2023-05-21 12:23:46 +0300

    done

--- django/cursor/main/views.py
+++ django/cursor/main/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
-from .models import MenuItem
+from .models import MenuItem, SliderItem
+from django.conf import settings
+import os
 
 
 def main(request):
     menu_items = MenuItem.objects.all()
-    return render(request, "index.html", {"menu_items": menu_items})
+    slider_image = SliderItem.objects.all()
+    return render(request, "index.html", {"menu_items": menu_items, "slider_image": slider_image})
